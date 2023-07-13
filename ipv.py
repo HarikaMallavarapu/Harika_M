75eee3b7d715fd0f3f1a72de5138e5c99814e2c2
HarikaMallavarapu <139160051+HarikaMallavarapu@users.noreply.github.com>
2023-07-13 09:57:35 +0530

    Update ipv.py

--- ipv.py
+++ ipv.py
@@ -1,16 +1,37 @@
-def validate_ip_regex(ip_address):
-   match = re.match(
-       r"[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}", "127.0.0.1", ip_address)
+import re
+str=input('Please enter a IP address\n')
+ipv4=str.split(".")
+ipv6=str.split(":")
 
-   if not bool(match):
-       print(f"The IP address {ip_address} is not valid")
-       return False
+ipv4_len=0
+ipv6_len=0
 
-   bytes = ip_address.split(".")
+ipv4_len=len(ipv4)
+ipv6_len=len(ipv6)
 
-   for ip_byte in bytes:
-       if int(ip_byte) < 0 or int(ip_byte) > 255:
-           print(f"The IP address {ip_address} is not valid")
-           return False
-   print(f"The IP address {ip_address} is valid")
-   return True
+if(ipv6_len==0 and ipv4_len>0):
+    check1=0
+    for i in ipv4:
+        if(0<=int(i)<=255):
+            check1=1
+        else:
+            check1=0
+elif(ipv4_len<4 and ipv6_len==8):
+    check2=0
+    for i in ipv6:
+        if(re.match(r'^[0-9a-fA-F]+$', i)):
+            check2=1
+        elif(i.isdigit()):
+            check2=1
+        else:
+            check2=0
+else:
+    check1=0
+    check2=0
+
+if(ipv4_len==4 and check1==1):
+    print('IPv4')
+elif(ipv6_len==8 and check2==1):
+    print('IPv6')
+else:
+    print('Neither')
